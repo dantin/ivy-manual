8a8f8916167ff9846fb7880f4b6bfec3422fc410
David Ding <chengjie.ding@gmail.com>
2014-03-03 16:20:18 +0800

    简单的Ivy文件

---
+++ samples/hello-ivy/src/example/Hello.java
@@ -0,0 +1,34 @@
+package example;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.lang.WordUtils;
+
+/**
+ * Simple example to show how easy it is to retrieve transitive libs with ivy !!! 
+ */
+public final class Hello {
+    public static void main(String[] args) throws Exception {
+        Option msg = OptionBuilder.withArgName("msg")
+            .hasArg()
+            .withDescription("the message to capitalize")
+            .create("message");
+        Options options = new Options();
+        options.addOption(msg);
+        
+        CommandLineParser parser = new GnuParser();
+        CommandLine line = parser.parse(options, args);
+        
+        String  message = line.getOptionValue("message", "hello ivy !");
+        System.out.println("standard message : " + message);
+        System.out.println("capitalized by " + WordUtils.class.getName() 
+			   + " : " + WordUtils.capitalizeFully(message));
+    }
+    
+    private Hello() {
+    }
+}
