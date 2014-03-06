33289f8dd40d28d7d1160ce47b2928c73f31d043
David Ding <chengjie.ding@gmail.com>
2014-03-06 15:59:35 +0800

    查询某一目录下所有文件或某一文件集合的总大小

---
+++ samples/multi-project/projects/size/src/size/FileSize.java
@@ -0,0 +1,28 @@
+package size;
+
+import version.Version;
+import java.util.Collection;
+import java.util.Iterator;
+import java.io.File;
+
+public final class FileSize {
+    static {
+	Version.register("size");
+    }
+
+    public static long totalSize(File dir) {
+	return totalSize(list.ListFile.list(dir));
+    }
+  
+    public static long totalSize(Collection files) {
+	long total = 0;
+	for (Iterator it = files.iterator(); it.hasNext();) {
+	    File f = (File) it.next();
+	    total += f.length();
+	}
+	return total;
+    }  
+  
+    private FileSize() {
+    }
+}
